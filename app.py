0c6d67ec496503b55f9fb27eb5add492ed431ae5
Thanatchon Chongmankhong <thanatchonchongmankhong@Thanatchons-MacBook-Air.local>
2022-04-22 15:13:14 +0700

    update

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ with st.sidebar:
         index = 1,
     )
 
-pdf_html = """<a href="file:///bot.or.th/cfs/FILESERV/DA_WORKSPACE/workstation/GLOBAL_WS/ria/shared/Assessingcrises.pdf" class="card-link">PDF</a> <a href='#linkto_top' class="card-link">Link to top</a> <a href='#linkto_bottom' class="card-link">Link to bottom</a>"""
+pdf_html = """<a href=file://bot.or.th/cfs/FILESERV/DA_WORKSPACE/workstation/GLOBAL_WS/ria/shared/Assessingcrises.pdf>PDF</a> <a href='#linkto_top' class="card-link">Link to top</a> <a href='#linkto_bottom' class="card-link">Link to bottom</a>"""
 
 st.markdown("""
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
